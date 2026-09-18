219dafa4b3185918b487b2165d73c01bc48315b4
gadhuli <hridoyff974@gmail.com>
2026-09-18 22:43:07 +0600

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ypmmdytgfuhydzacjbkw.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_ZhmwEV_6UngmzzxcJmETgw_smrA7qDL";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
