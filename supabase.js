b680295d41eef1611d2873e4413b5f46040c69a7
gadhuli <hridoyff974@gmail.com>
2026-09-20 22:02:02 +0600

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,8 +1,11 @@
-const SUPABASE_URL = "https://ypmmdytgfuhydzacjbkw.supabase.co";
+const SUPABASE_URL =
+"https://ypmmdytgfuhydzacjbkw.supabase.co";
 
-const SUPABASE_KEY = "sb_publishable_ZhmwEV_6UngmzzxcJmETgw_smrA7qDL";
+const SUPABASE_KEY =
+"sb_publishable_ZhmwEV_6UngmzzxcJmETgw_smrA7qDL";
 
-const supabase = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_KEY
-);
+window.gadhuliSupabase =
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
